2711c887c673bee45c41d3b2ef39250ed0a8c976
Ben McHone <bmchone@sourceallies.com>
2022-11-21 13:12:48 -0600

    Base solution files for all days

---
+++ day_5/spec/solution_spec.rb
@@ -0,0 +1,18 @@
+def solution_part_1
+	return 1
+end
+
+def solution_part_2
+	return 1
+end
+
+describe "Day 5" do
+	
+	it "Part 1 should pass" do
+		expect(solution_part_1()).to eq(1)
+	end
+
+	it "Part 2 should pass" do
+		expect(solution_part_2()).to eq(1)
+	end
+end
