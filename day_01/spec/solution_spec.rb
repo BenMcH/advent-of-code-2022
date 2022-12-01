f0ee79b516b9ffca532d1597e1b834543cb924a5
Ben McHone <ben@mchone.dev>
2022-12-01 05:08:41 +0000

    AOC 2022 Day 1

--- day_01/spec/solution_spec.rb
+++ day_01/spec/solution_spec.rb
@@ -1,21 +1,27 @@
-test_input = File.read('./test-input.txt')
+test_input = File.read('./test-input.txt').to_s
 real_input = File.read('./input')
 
-def solution_part_1
-	return 1
+def solution_part_1(input)
+	elves = input.split("\n\n").map{|elf| elf.split("\n").map(&:to_i).sum}
+
+	return elves.max
 end
 
-def solution_part_2
-	return 1
+def solution_part_2(input)
+	elves = input.split("\n\n").map{|elf| elf.split("\n").map(&:to_i).sum}
+
+	return elves.sort[-3..].sum
 end
 
 describe "Day 1" do
 	
 	it "Part 1 should pass" do
-		expect(solution_part_1()).to eq(1)
+		expect(solution_part_1(test_input)).to eq(24000)
+		p "Part 1 solution #{solution_part_1(real_input)}"
 	end
 
 	it "Part 2 should pass" do
-		expect(solution_part_2()).to eq(1)
+		expect(solution_part_2(test_input)).to eq(45000)
+		p "Part 2 solution #{solution_part_2(real_input)}"
 	end
 end
