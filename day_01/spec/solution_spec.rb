2d5863581d1255adc211d8b9ddf7e2d06eca8041
Ben McHone <ben@mchone.dev>
2022-12-02 03:54:28 +0000

    Slight golf of day 1

--- day_01/spec/solution_spec.rb
+++ day_01/spec/solution_spec.rb
@@ -1,27 +1,19 @@
 test_input = File.read('./test-input.txt').to_s
 real_input = File.read('./input')
 
-def calculate_elf_carry_weights(input)
-	return input.split("\n\n").map{|elf| elf.split("\n").map(&:to_i).sum}
-end
-
-def solution_part_1(input)
-	return calculate_elf_carry_weights(input).max
-end
-
-def solution_part_2(input)
-	return calculate_elf_carry_weights(input).sort[-3..].sum
+def calculate_elf_carry_weights(input, top_elves)
+	return input.split("\n\n").map{|elf| elf.lines.map(&:to_i).sum}.sort.last(top_elves).sum
 end
 
 describe "Day 1" do
 	
-	it "Part 1 should pass" do
-		expect(solution_part_1(test_input)).to eq(24000)
-		p "Part 1 solution #{solution_part_1(real_input)}"
+	it "Part 1" do
+		expect(calculate_elf_carry_weights(test_input, 1)).to eq(24_000)
+		p "Part 1 #{calculate_elf_carry_weights(real_input, 1)}"
 	end
 
-	it "Part 2 should pass" do
-		expect(solution_part_2(test_input)).to eq(45000)
-		p "Part 2 solution #{solution_part_2(real_input)}"
+	it "Part 2" do
+		expect(calculate_elf_carry_weights(test_input, 3)).to eq(45_000)
+		p "Part 2 #{calculate_elf_carry_weights(real_input, 3)}"
 	end
 end
