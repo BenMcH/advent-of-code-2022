56e073654877aaac3cbd743e34c6764ade47d036
Ben McHone <ben@mchone.dev>
2022-12-03 05:18:07 +0000

    Day 3

--- day_03/spec/solution_spec.rb
+++ day_03/spec/solution_spec.rb
@@ -1,21 +1,78 @@
-test_input = File.read('./test-input.txt')
-real_input = File.read('./input')
+test_input = File.read('./test-input.txt').lines
+real_input = File.read('./input').lines
 
-def solution_part_1
-	return 1
+RuckSack = Struct.new(
+	:compartment_1, 
+	:compartment_2,
+	:all
+)
+
+PRIORITIES = ' '.concat(('a'..'z').to_a.join('')).concat(('A'..'Z').to_a.join(''))
+
+def make_ruck(input)
+	len = input.length
+
+	a = input[0..len/2-1].split('')
+	b = input[len/2..-1].split('')
+
+	return RuckSack.new(a, b, input.split(''))
 end
 
-def solution_part_2
-	return 1
+def solution_part_1(input)
+	rucks = input.map do |line|
+		ruck = make_ruck(line)
+
+		ruck.compartment_1.intersection(ruck.compartment_2)[0]
+	end
+
+	ans = 0
+
+	rucks.each do |ruc|
+		ans += PRIORITIES.index(ruc)
+	end
+
+	return ans
+end
+
+def find_intersection(input_arr)
+	ans, *list = *input_arr
+
+	list.each do |l|
+		ans = ans.intersection(l)
+	end
+
+	return ans
+end
+
+def solution_part_2(input)
+	rucks = input.map do |line|
+		ruck = make_ruck(line)
+
+		ruck.all
+	end
+
+	ans = 0
+
+	rucks.each_slice(3) do |rucs|
+		badge = find_intersection(rucs)[0]
+
+		ans += PRIORITIES.index(badge)
+	end
+
+	ans
 end
 
 describe "Day 3" do
 	
 	it "Part 1 should pass" do
-		expect(solution_part_1()).to eq(1)
+		expect(solution_part_1(test_input)).to eq(157)
+
+		p "Part 1 #{solution_part_1(real_input)}"
 	end
 
 	it "Part 2 should pass" do
-		expect(solution_part_2()).to eq(1)
+		expect(solution_part_2(test_input)).to eq(70)
+		
+		p "Part 2 #{solution_part_2(real_input)}"
 	end
 end
