635ad12359fd8d2520bbcf04e546520c4b7c7993
Ben McHone <ben@mchone.dev>
2022-12-04 05:24:47 +0000

    Made more ruby-esque

--- day_04/spec/solution_spec.rb
+++ day_04/spec/solution_spec.rb
@@ -2,7 +2,7 @@ test_input = File.read('./test-input.txt')
 real_input = File.read('./input')
 
 def parse_elves(input)
-	input.lines.flat_map do |str|
+	input.lines.map do |str|
 		str.split(',').map do |elf_sections|
 			start, end_section = *elf_sections.split('-')
 
@@ -14,29 +14,21 @@ end
 def solution_part_1(input)
 	elf_pairs = parse_elves(input)
 
-	count = 0
-
-	elf_pairs.each_slice(2) do |pair|
+	elf_pairs.count do |pair|
 		e1, e2 = *pair
 
-		count += 1 if e1.cover?(e2) || e2.cover?(e1)
+		e1.cover?(e2) || e2.cover?(e1)
 	end
-
-	return count
 end
 
 def solution_part_2(input)
 	elf_pairs = parse_elves(input)
 
-	count = 0
-
-	elf_pairs.each_slice(2) do |pair|
+	elf_pairs.count do |pair|
 		e1, e2 = *pair
 
-		count += 1 if e1.cover?(e2.first) || e2.cover?(e1.first)
+		e1.cover?(e2.first) || e2.cover?(e1.first)
 	end
-
-	return count
 end
 
 describe "Day 4" do
