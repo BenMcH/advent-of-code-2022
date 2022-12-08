353dd6be962ac70e2b3abb633be5a00db4f593b7
Ben McHone <ben@mchone.dev>
2022-12-08 05:49:11 +0000

    Use/abuse of ruby array methods

--- day_08/spec/solution_spec.rb
+++ day_08/spec/solution_spec.rb
@@ -7,22 +7,17 @@ def solution_part_1(input)
 	count = 0
 
 	grid.each.with_index do |arr, i|
-		if i == 0 || i == grid.length - 1
-			count += arr.length
-			next
-		end
-		arr.each.with_index do |elem, j|
+		count += arr.each_with_index.count do |elem, j|
 			visible = false
 
-			visible ||= j == 0 || j == arr.length - 1 # On Edge
+			visible ||= i == 0 || i == grid.length - 1 # On top or bottom
+			visible ||= j == 0 || j == arr.length - 1 # On Side Edge
 			visible ||= (0...i).all? { |new_i| grid[new_i][j] < elem } # Visible on the left
 			visible ||= ((i+1)...grid.length).all? { |new_i| grid[new_i][j] < elem } # Visible on the right
 			visible ||= (0...j).all? { |new_j| grid[i][new_j] < elem } # Visible on top
-			visible ||= ((j+1)...arr.length).all? { |new_j| grid[i][new_j] < elem }
+			visible ||= ((j+1)...arr.length).all? { |new_j| grid[i][new_j] < elem } # Visible on bottom
 
-			if visible
-				count += 1
-			end
+			visible	
 		end
 	end
 
@@ -32,56 +27,52 @@ end
 def solution_part_2(input)
 	grid = input.lines.map{|l| l.strip.chars.map(&:to_i)}
 
-	highest_scenic_score = 0
+	grid.map.with_index do |arr, i|
+		if i == 0 || i == grid.length - 1 # On edge, at least 1 value is 0
+			0
+		else
+			arr.map.with_index do |elem, j|
+				scores = []
+				score = 0
+				
+				unless j == 0 || j == arr.length
+					for new_i in (i-1).downto(0)
+						score += 1
+						break if grid[new_i][j] >= grid[i][j]
+					end
 
-	grid.each.with_index do |arr, i|
-		next if i == 0 || i == grid.length - 1 # On edge, at least 1 value is 0
+					scores << score
+					score = 0
+					
+					for new_i in (i+1)...grid.length
+						score += 1
+						break if grid[new_i][j] >= grid[i][j]
+					end
 
-		arr.each.with_index do |elem, j|
-			next if j == 0 || j == arr.length # On edge, at least 1 value is 0
+					scores << score
+					score = 0
+					
+					for new_j in (j-1).downto(0)
+						score += 1
+						break if grid[i][new_j] > grid[i][j]
+					end
 
-			scores = []
-			score = 0
-			
-			for new_i in (i-1).downto(0)
-				score += 1
-				break if grid[new_i][j] >= grid[i][j]
-			end
+					scores << score
+					score = 0
+					
+					for new_j in (j+1)...arr.length
+						score += 1
+						break if grid[i][new_j] >= grid[i][j]
+					end
 
-			scores << score
-			score = 0
-			
-			for new_i in (i+1)...grid.length
-				score += 1
-				break if grid[new_i][j] >= grid[i][j]
-			end
+					scores << score
+					score = scores.reduce(1) {|product, value| product * value}
+				end
 
-			scores << score
-			score = 0
-			
-			for new_j in (j-1).downto(0)
-				score += 1
-				break if grid[i][new_j] > grid[i][j]
-			end
-
-			scores << score
-			score = 0
-			
-			for new_j in (j+1)...arr.length
-				score += 1
-				break if grid[i][new_j] >= grid[i][j]
-			end
-
-			scores << score
-			score = scores.reduce(1) {|product, value| product * value}
-
-			highest_scenic_score = score if score > highest_scenic_score
+				score
+			end.max
 		end
-
-		highest_scenic_score
-	end
-
-	return highest_scenic_score
+	end.max
 end
 
 describe "Day 8" do
