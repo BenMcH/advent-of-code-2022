acf3c004fc0d5021e43b5992ac9cb61f473c79ff
Ben McHone <ben@mchone.dev>
2022-12-11 05:55:04 +0000

    Day 11

--- day_11/spec/solution_spec.rb
+++ day_11/spec/solution_spec.rb
@@ -1,21 +1,134 @@
-test_input = File.read('./test-input.txt')
-real_input = File.read('./input')
+require "net/http"
 
-def solution_part_1
-	return 1
+test_input = File.read('./test-input.txt')
+real_input = if File.exists?('./input')
+	File.read('./input')
+else
+	p "Requesting"
+	res = Net::HTTP.get_response(URI("https://adventofcode.com/2022/day/11/input"), {Cookie: "session=#{File.read("/home/vscode/.adventofcode.session")}", "User-Agent" => "Ben McHone <ben@mchone.dev>"})
+
+	File.write('./input', res.body)
+
+	res.body
 end
 
-def solution_part_2
-	return 1
+part_1_expected = 10605
+part_2_expected = 2713310158
+
+Monkey = Struct.new(
+	:starting_items,
+	:operation,
+	:test,
+	:true_action,
+	:false_action,
+	keyword_init: true
+)
+
+def parse_monkey(monkey_input)
+	id, starting, operation, test_cond, true_action, false_action = *monkey_input.split("\n").map(&:strip)
+
+	operation = operation.split("=")[1]
+
+	operand, action, operand_1 = operation.split(" ")
+
+	Monkey.new(
+		starting_items: starting.scan(/\d+/).map(&:to_i),
+		operation: [operand, action, operand_1],
+		test: test_cond.scan(/\d+/)[0].to_i,
+		true_action: true_action.scan(/\d+/)[0].to_i,
+		false_action: false_action.scan(/\d+/)[0].to_i
+	)
+end
+
+def inspect_item(monkey, extra_worry, max_test)
+	worry = monkey.starting_items[0]
+
+	operand, action, operand_1 = monkey.operation[0], monkey.operation[1], monkey.operation[2]
+
+	operand = worry if operand == "old"
+	operand_1 = worry if operand_1 == "old"
+
+	worry = operand.to_i + operand_1.to_i if action == "+"
+	worry = operand.to_i * operand_1.to_i if action == "*"
+	worry = worry % max_test
+	worry = worry.div(3) unless extra_worry
+
+
+	monkey.starting_items[0] = worry
+end
+
+def test_item(monkey)
+	monkey["#{monkey.starting_items[0] % monkey.test == 0}_action"]
+end
+
+def round(monkeys, extra_worry = true)
+	max_test = monkeys.map(&:test).reduce(1){|acc, v| v * acc}
+	operations = []
+	monkeys.each do |monke|
+		operations << 0
+		until monke.starting_items.length == 0
+			inspect_item(monke, extra_worry, max_test)
+			operations[-1] += 1
+			next_monke = test_item(monke)
+
+			monkeys[next_monke].starting_items << monke.starting_items[0]
+			monke.starting_items = monke.starting_items[1..-1]
+		end
+	end
+
+	operations
+end
+
+def solution_part_1(input)
+	monkeys = input.strip.split("\n\n").map{|m| parse_monkey(m)}
+
+	operations = nil
+
+	20.times do
+		ops = round(monkeys, false)
+
+		if operations
+			ops.each_with_index do |val, i|
+				operations[i] += val
+			end
+		else
+			operations = ops
+		end
+	end
+
+	operations.sort.last(2).reduce(1) {|acc, val| acc * val}
+end
+
+def solution_part_2(input)
+	monkeys = input.strip.split("\n\n").map{|m| parse_monkey(m)}
+
+	operations = nil
+
+	10_000.times do |i|
+
+		ops = round(monkeys, true)
+
+		if operations
+			ops.each_with_index do |val, i|
+				operations[i] += val
+			end
+		else
+			operations = ops
+		end
+	end
+
+	operations.sort.last(2).reduce(1) {|acc, val| acc * val}
 end
 
 describe "Day 11" do
 	
 	it "Part 1 should pass" do
-		expect(solution_part_1()).to eq(1)
+		expect(solution_part_1(test_input)).to eq(part_1_expected)
+		p solution_part_1(real_input)
 	end
 
 	it "Part 2 should pass" do
-		expect(solution_part_2()).to eq(1)
+		expect(solution_part_2(test_input)).to eq(part_2_expected)
+		p solution_part_2(real_input)
 	end
 end
