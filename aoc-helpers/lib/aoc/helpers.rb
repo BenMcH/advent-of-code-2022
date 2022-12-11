c112e95b73f8719e15d38fa17573228948309634
Ben McHone <ben@mchone.dev>
2022-12-11 23:02:30 +0000

    Setup day 12; Prevent saving bad input

--- aoc-helpers/lib/aoc/helpers.rb
+++ aoc-helpers/lib/aoc/helpers.rb
@@ -10,8 +10,13 @@ module Aoc
       input = if File.exists?('./input')
         File.read('./input')
       else
-        p "Requesting"
+        puts "Requesting"
         res = Net::HTTP.get_response(URI("https://adventofcode.com/#{year}/day/#{day}/input"), {Cookie: "session=#{session}", "User-Agent" => "Ben McHone <ben@mchone.dev>"})
+        body = res.body
+
+        if body.strip == "Please don't repeatedly request this endpoint before it unlocks! The calendar countdown is synchronized with the server time; the link will be enabled on the calendar the instant this puzzle becomes available."
+          throw Error.new("Puzzle input not open")
+        end
 
         File.write('./input', res.body)
 
