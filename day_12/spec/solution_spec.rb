528e2919f81a63f8b31593668e19873aaa927a47
Ben McHone <ben@mchone.dev>
2022-12-13 01:07:49 +0000

    Final edits

--- day_12/spec/solution_spec.rb
+++ day_12/spec/solution_spec.rb
@@ -33,45 +33,42 @@ def walk(map, location, destination, part_2 = false)
 	distance_map = {}
 
 	until neighbors.empty?
-		n = neighbors.pop
-
-		distance, loc = *n
+		distance, loc = *neighbors.pop
 
 		distance_map[loc] ||= inf
 
-		if distance_map[loc] <= distance
-			next
-		end
+		next if distance_map[loc] <= distance
 
 		distance_map[loc] = distance
 
 		row, col = *loc
 
+		new_dist = distance+1
 		if row > 0 && map[row-1][col] <= map[row][col] + 1
 			nloc = [row-1, col]
-			if !distance_map[nloc] || distance_map[nloc] > distance + 1
-				neighbors << [distance + 1, nloc]
+			if !distance_map[nloc] || distance_map[nloc] > new_dist
+				neighbors << [new_dist, nloc]
 				neighbors[-1][0] = 0 if map[nloc[0]][nloc[1]] == 0 && part_2
 			end
 		end
 		if row+1 < map.length && map[row+1][col] <= map[row][col]+1
 			nloc = [row+1, col]
-			if !distance_map[nloc] || distance_map[nloc] > distance + 1
-				neighbors << [distance + 1, nloc] 
+			if !distance_map[nloc] || distance_map[nloc] > new_dist
+				neighbors << [new_dist, nloc] 
 				neighbors[-1][0] = 0 if map[nloc[0]][nloc[1]] == 0 && part_2
 			end
 		end
 		if col > 0 && map[row][col-1] <= map[row][col]+1
 			nloc = [row, col - 1]
-			if !distance_map[nloc] || distance_map[nloc] > distance + 1
-				neighbors << [distance + 1, nloc] 
+			if !distance_map[nloc] || distance_map[nloc] > new_dist
+				neighbors << [new_dist, nloc] 
 				neighbors[-1][0] = 0 if map[nloc[0]][nloc[1]] == 0 && part_2
 			end
 		end
 		if col+1 < map[0].length && map[row][col+1] <= map[row][col]+1
 			nloc = [row, col + 1]
-			if !distance_map[nloc] || distance_map[nloc] > distance + 1
-				neighbors << [distance + 1, nloc] 
+			if !distance_map[nloc] || distance_map[nloc] > new_dist
+				neighbors << [new_dist, nloc] 
 				neighbors[-1][0] = 0 if map[nloc[0]][nloc[1]] == 0 && part_2
 			end
 		end
@@ -96,13 +93,11 @@ describe "Day 12" do
 	
 	it "Part 1 should pass" do
 		expect(solution_part_1(test_input)).to eq(part_1_expected)
-		p "Passed"
 		p solution_part_1(real_input)
 	end
 
 	it "Part 2 should pass" do
 		expect(solution_part_2(test_input)).to eq(part_2_expected)
-		p "Passed"
 		p solution_part_2(real_input)
 	end
 end
